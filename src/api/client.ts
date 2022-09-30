af60f1468336dfbb118682752543f1dd0d15ab59
Karol Moroz <karol@moroz.dev>
2022-09-30 19:40:30 +0200

    Use GraphQL URI from env vars

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/api",
+  uri: import.meta.env.VITE_GRAPHQL_URL ?? "http://localhost:4000/api",
   cache: new InMemoryCache(),
   credentials: "include"
 });
